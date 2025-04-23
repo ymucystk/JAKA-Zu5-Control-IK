87c0f232cd758f41119880a083f373a6f7545e4b
Yamauchi Yoshitaka <ymucystk@ucl.nuee.nagoya-u.ac.jp>
2025-04-23 11:30:31 +0900

    joint slerp raw data

--- src/app/home.js
+++ src/app/home.js
@@ -234,12 +234,12 @@ export default function Home(props) {
   // 移動をゆっくりさせる仕組み
   //React.useEffect(()=>{
   const joint_slerp = () => {
-    let recursive_flg = false
+    let raw_data = 0
     for(let i=0; i<rotate_table.length; i=i+1){
       const current_table = rotate_table[i]
       const current_object3D = object3D_table[i]
+      raw_data = raw_data + current_table.length
       if(current_object3D !== undefined && current_table.length > 0){
-        recursive_flg = true
         const current_data = current_table[0]
         if(current_data.first){
           current_data.first = false
@@ -264,7 +264,7 @@ export default function Home(props) {
         }
       }
     }
-    if(recursive_flg){
+    if(raw_data > 0){
       setTimeout(()=>{joint_slerp()},0)
     }
   }
